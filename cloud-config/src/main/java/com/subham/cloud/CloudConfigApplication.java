0f2343842d4e67499e2395b77f97e424c1f9823e
Subham Sarat Swain <subhamswain2100@gmail.com>
2021-07-15 00:25:24 +0530

    cloud configuration added for generalising the access to eureka server and generating a git repo for the cloud config storing generalised data

---
+++ cloud-config/src/main/java/com/subham/cloud/CloudConfigApplication.java
@@ -0,0 +1,16 @@
+package com.subham.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigApplication.class, args);
+	}
+
+}
---
+++ cloud-config/src/test/java/com/subham/cloud/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subham.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
