a249da7b96c239759bfb86fa853e51bf21e5f48a
Subham Sarat Swain <subhamswain2100@gmail.com>
2021-07-15 00:47:44 +0530

    fallback method controller added for fault tolerance ,application should not stop even if one of the microservices stopped running,zipkin config also added

---
+++ cloudgateway/src/main/java/com/subham/cloudgateway/FallBackmethodcontroller.java
@@ -0,0 +1,23 @@
+package com.subham.cloudgateway;
+
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableCircuitBreaker
+public class FallBackmethodcontroller {
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User Service is taking longer than Expected." +
+                " Please try again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Department Service is taking longer than Expected." +
+                " Please try again later";
+
+
+    }
+}
