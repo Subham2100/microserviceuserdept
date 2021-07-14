4eeec141b561a04ea66b31f91faff8051993e33e
Subham Sarat Swain <subhamswain2100@gmail.com>
2021-07-15 00:17:11 +0530

    dept service added

---
+++ department_service/src/main/java/com/subham/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.subham.department.repository;
+
+import com.subham.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
